f899ba3f93a2a8b7c3f0ab6fb7334768f804701e
chhong <pnci1029@gmail.com>
2025-05-06 14:44:37 +0900

    커뮤니티 등록 추가

--- src/components/community/hooks/useCommunityActions.ts
+++ src/components/community/hooks/useCommunityActions.ts
@@ -1,21 +1,17 @@
 import {useDispatch} from "react-redux";
-import {useCallback, useState} from "react";
+import {useCallback} from "react";
 import {CommunityPostDTO} from "../../../types/community";
 import {postArticleAsync} from "../../../slice/communitySlice";
 
 export function useCommunityActions() {
     const dispatch = useDispatch<any>();
-    const [, setIsLoading] = useState<boolean>(false);
 
     const postArticle = useCallback(
         async (dto: CommunityPostDTO) => {
-            setIsLoading(true);
             try {
                  await dispatch(postArticleAsync(dto));
             } catch (e) {
                 console.error("데이터 로딩 실패:", e);
-            } finally {
-                setIsLoading(false);
             }
         },
         [dispatch]
