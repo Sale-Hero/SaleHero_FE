d31cb10e74ec9353c90050325b7946cb60b81da1
chhong <pnci1029@gmail.com>
2025-05-17 12:58:39 +0900

    관리자 api 추가

--- src/api/AdminApi.ts
+++ src/api/AdminApi.ts
@@ -1,11 +1,11 @@
 import {MainApi} from "../api/MainApi";
-import {AdminNewsLetterSearchDTO} from "../types/adminNewsLetter";
-import {RawNewsLetterSearchDTO} from "../types/rawNewsLetter";
+import {AdminNewsLetterSearchDTO, NewsLetterDeleteDTO} from "../types/adminNewsLetter";
+import {RawNewsLetterPutDTO, RawNewsLetterSearchDTO} from "../types/rawNewsLetter";
 
 export class AdminApi {
     static url = `${process.env.REACT_APP_BASE_URL}/admin`;
 
-    static getCurrentCount = (token:string) => () =>
+    static getCurrentCount = (token: string) => () =>
         MainApi.api.get(`${AdminApi.url}/common/count`, {
             // headers: {
             //     'Authorization': `Bearer ${token}`
@@ -13,10 +13,17 @@ export class AdminApi {
         });
 
     // 뉴스 레터
-
     static getAdminNewsLetters = (dto: AdminNewsLetterSearchDTO) => () =>
         MainApi.api.get(`${AdminApi.url}/news${MainApi.toParamStringFromObject(dto)}`);
 
+
+    // 뉴스레터 로우데이터 ㅡㅡㅡㅡㅡㅡㅡ
     static getRawNewsLetters = (dto: RawNewsLetterSearchDTO) => () =>
         MainApi.api.get(`${AdminApi.url}/raw${MainApi.toParamStringFromObject(dto)}`);
+
+    static modifyRawNewsLetter = (dto: RawNewsLetterPutDTO) => () =>
+        MainApi.api.put(`${AdminApi.url}/raw`, dto);
+
+    static deleteRawNewsLetter = (dto: NewsLetterDeleteDTO) => () =>
+        MainApi.api.delete(`${AdminApi.url}/raw`, {data: dto});
 }
--- src/slice/AdminSlice.ts
+++ src/slice/AdminSlice.ts
@@ -1,8 +1,8 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {executePromise} from "../util/sliceUtil";
 import {AdminApi} from "../api/AdminApi";
-import {AdminNewsLetterSearchDTO} from "../types/adminNewsLetter";
-import {RawNewsLetterSearchDTO} from "../types/rawNewsLetter";
+import {AdminNewsLetterSearchDTO, NewsLetterDeleteDTO} from "../types/adminNewsLetter";
+import {RawNewsLetterPutDTO, RawNewsLetterSearchDTO} from "../types/rawNewsLetter";
 
 export const getCurrentCountAsync = createAsyncThunk("admin/getCurrentCount",
     (token:string) => executePromise(AdminApi.getCurrentCount(token)));
@@ -10,5 +10,14 @@ export const getCurrentCountAsync = createAsyncThunk("admin/getCurrentCount",
 export const getAdminNewsLettersAsync = createAsyncThunk("admin/getAdminNewsLetters",
     (dto: AdminNewsLetterSearchDTO) => executePromise(AdminApi.getAdminNewsLetters(dto)));
 
+
+
+// 뉴스레터 로우데이터 ㅡㅡㅡㅡㅡㅡㅡ
 export const getRawNewsLettersAsync = createAsyncThunk("admin/getRawNewsLetters",
     (dto: RawNewsLetterSearchDTO) => executePromise(AdminApi.getRawNewsLetters(dto)));
+
+export const modifyRawNewsLetterAsync = createAsyncThunk("admin/modifyRawNewsLetter",
+    (dto: RawNewsLetterPutDTO) => executePromise(AdminApi.modifyRawNewsLetter(dto)));
+
+export const deleteRawNewsLetterAsync = createAsyncThunk("admin/deleteRawNewsLetter",
+    (dto: NewsLetterDeleteDTO) => executePromise(AdminApi.deleteRawNewsLetter(dto)));
