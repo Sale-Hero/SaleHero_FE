94063c69f013a694d380ac2ebddfc5a6657ce973
chhong <pnci1029@gmail.com>
2025-07-13 15:57:49 +0900

    커스텀 훅 변경 및 DTO이름 수정

--- src/components/admin/hooks/useAdminArticleActions.ts
+++ src/components/admin/hooks/useAdminArticleActions.ts
@@ -1,17 +1,17 @@
 import {useDispatch} from "react-redux";
 import {useCallback, useState} from "react";
-import {ArticleDTO, ArticleDeleteDTO} from "../../../types/adminArticle";
+import {AdminArticlePostDTO, ArticleDeleteDTO} from "../../../types/adminArticle";
+import {postAdminArticleAsync} from "../../../slice/AdminSlice";
 
 export function useArticleActions() {
     const dispatch = useDispatch<any>();
     const [loading, setLoading] = useState(false);
 
     // 실제로는 dispatch(saveArticleAsync(formData, id)) 등으로 API 연동 필요
-    const saveArticle = useCallback(async (formData: ArticleDTO, articleId?: number) => {
+    const saveArticle = useCallback(async (formData: AdminArticlePostDTO) => {
         setLoading(true);
         try {
-            // TODO: 실제 API 연동 필요
-            // 예시: await dispatch(saveArticleAsync({formData, articleId}));
+            await dispatch(postAdminArticleAsync(formData));
             return true;
         } catch (e) {
             return false;
@@ -39,4 +39,4 @@ export function useArticleActions() {
         deleteArticles,
         loading
     };
-} 
+}
