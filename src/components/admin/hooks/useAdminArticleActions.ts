a917d59a4c0b2021925d5977babd627378c688c3
chhong <pnci1029@gmail.com>
2025-07-18 21:58:37 +0900

    api 연동 추가

--- src/components/admin/hooks/useAdminArticleActions.ts
+++ src/components/admin/hooks/useAdminArticleActions.ts
@@ -1,13 +1,12 @@
 import {useDispatch} from "react-redux";
 import {useCallback, useState} from "react";
 import {AdminArticlePostDTO, ArticleDeleteDTO} from "../../../types/adminArticle";
-import {postAdminArticleAsync} from "../../../slice/AdminSlice";
+import {deleteAdminArticleAsync, postAdminArticleAsync, putAdminArticleAsync} from "../../../slice/AdminSlice";
 
 export function useArticleActions() {
     const dispatch = useDispatch<any>();
     const [loading, setLoading] = useState(false);
 
-    // 실제로는 dispatch(saveArticleAsync(formData, id)) 등으로 API 연동 필요
     const saveArticle = useCallback(async (formData: AdminArticlePostDTO) => {
         setLoading(true);
         try {
@@ -20,12 +19,24 @@ export function useArticleActions() {
         }
     }, [dispatch]);
 
-    // 실제로는 dispatch(deleteArticlesAsync(dto)) 등으로 API 연동 필요
-    const deleteArticles = useCallback(async (dto: ArticleDeleteDTO) => {
+    const updateArticle = useCallback(
+        async ({formData,articleId}: {formData:AdminArticlePostDTO, articleId: number}) => {
         setLoading(true);
         try {
-            // TODO: 실제 API 연동 필요
-            // 예시: await dispatch(deleteArticlesAsync(dto));
+            await dispatch(putAdminArticleAsync({dto: formData, articleId}));
+            return true;
+        } catch (e) {
+            return false;
+        } finally {
+            setLoading(false);
+        }
+    }, [dispatch]);
+
+    const deleteArticles = useCallback(
+        async (dto: ArticleDeleteDTO) => {
+        setLoading(true);
+        try {
+            await dispatch(deleteAdminArticleAsync(dto));
             return true;
         } catch (e) {
             return false;
@@ -36,6 +47,7 @@ export function useArticleActions() {
 
     return {
         saveArticle,
+        updateArticle,
         deleteArticles,
         loading
     };
