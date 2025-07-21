d07f4bbebf1ac3c696605c33f03aaab2b6697f05
chhong <pnci1029@gmail.com>
2025-07-21 20:15:29 +0900

    상세 API 추가

--- src/api/ArticleApi.ts
+++ src/api/ArticleApi.ts
@@ -1,5 +1,6 @@
 import {ArticleSearchDTO} from "../types/adminArticle";
 import {MainApi} from "./MainApi";
+import {getUserArticleDetailAsync} from "../slice/ArticleSlice";
 
 export class ArticleApi {
     static url = `${process.env.REACT_APP_BASE_URL}/articles`;
@@ -7,4 +8,7 @@ export class ArticleApi {
 
     static getUserArticles = (dto: ArticleSearchDTO) => () =>
         MainApi.api.get(`${ArticleApi.url}${MainApi.toParamStringFromObject(dto)}`);
+
+    static getUserArticleDetail = (articleId: number) => () =>
+        MainApi.api.get(`${ArticleApi.url}/${articleId}`);
 }
--- src/slice/ArticleSlice.ts
+++ src/slice/ArticleSlice.ts
@@ -5,3 +5,6 @@ import {ArticleApi} from "../api/ArticleApi";
 
 export const getUserArticlesAsync = createAsyncThunk("user/getUserArticles",
     (dto: ArticleSearchDTO) => executePromise(ArticleApi.getUserArticles(dto)));
+
+export const getUserArticleDetailAsync = createAsyncThunk("user/getUserArticleDetail",
+    (articleId: number) => executePromise(ArticleApi.getUserArticleDetail(articleId)));
