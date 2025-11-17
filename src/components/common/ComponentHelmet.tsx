8a310ac6208ea3bf2b7c398d8762549227e0d288
chhong <pnci1029@gmail.com>
2025-11-17 21:24:06 +0900

    SEO 처리 세팅 (컴포넌트 헬멧 및 robots.txt 추가)

--- src/components/common/ComponentHelmet.tsx
+++ src/components/common/ComponentHelmet.tsx
@@ -2,11 +2,13 @@ import {Helmet} from "react-helmet-async";
 
 interface Args{
     title: string;
+    description?: string;
 }
-export function ComponentHelmet({title}:Args) {
+export function ComponentHelmet({title, description}:Args) {
     return(
         <Helmet>
             <title>{title}</title>
+            {description && <meta name="description" content={description} />}
         </Helmet>
     )
 }
